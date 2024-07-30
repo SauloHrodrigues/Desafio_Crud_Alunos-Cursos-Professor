d074d4c676700d7cf66fe6398edb716884a11afa
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-07-30 09:52:10 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/cursoadm/cursoadm/CursoadmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoadm.cursoadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoadmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
