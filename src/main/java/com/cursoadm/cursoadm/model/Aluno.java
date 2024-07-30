3a97f3f1e09a4017693a27b79a91173338659f11
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-07-30 10:56:24 -0300

    Entidade Aluno criada

---
+++ src/main/java/com/cursoadm/cursoadm/model/Aluno.java
@@ -0,0 +1,23 @@
+package com.cursoadm.cursoadm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String nomeDoAluno;
+    private List<Curso> cursos = new ArrayList<>();
+}
